001ed175d23b0d8eea42694bab95699a5ec8bd87
Viktor Senderov <vsenderov@gmail.com>
2018-03-11 11:06:55 +0200

    work on the db function factories

--- R/db-access-high-level.R
+++ R/db-access-high-level.R
@@ -9,39 +9,26 @@
 #' @examples
 #' p_query = "SELECT *
 #'         WHERE {
-#' ?id rdfs:label %label
+#' ?id2 rdfs:label %label. ?id2 rdfs:label %label2.
 #' }"
-#' grapdhb_lookup_id = query_function_constructor(p_query, access_options = graphdb)
+#'f = query_function_constructor(p_query, access_options = graphdb)
 #'
 #' @export
 query_function_constructor = function(p_query, access_options, prefixes = NA)
 {
-  # add prefixes to beginning
-  p_query = paste(prefix_serializer_sparql(prefixes))
-  params = list()   # 1. find what are the parameters (i.e. % strings)
+  p_query = paste(prefix_serializer_sparql(prefixes), p_query, sep = "\n\n")  # add prefixes to beginning
 
-  f = function(params)   # 2. This list will be the function arguments
-  {
+  params = (regmatches(
+    p_query,
+    gregexpr("%[[:alnum:]_]+", p_query)
+  )) # 1. find what are the parameters (i.e. % strings))
+
+  f = function(replacement){
+    browser()
     # 3. The function itself will replace the parameters in the p_query with values of its arguments
+    gsubfn(pattern = "%[[:alnum:]_]+", replacement = replacement, x = p_query)
     # for this I can look at the implementation of lookup_id in ropenbio
     # 4. The function will connect to the database and submit the query
   }
-
-
-
-
-
 }
 
-
-
-openbiodiv_lookup_id = query_function_constructor(s, lookup_pquery, prefix_list)
-
-openbiodiv_lookup_id(label = "Teodor Georgiev")
-
-
-```
-"SELECT *
-WHERE {
-?id rdfs:label "Teodor Georgiev"
-}
