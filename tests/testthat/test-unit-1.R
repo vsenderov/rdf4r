9cd59ab154d1759e91d35c05b995b10e9ec9c8ea
Viktor Senderov <vsenderov@gmail.com>
2018-06-20 10:59:07 +0300

    moar testz

--- tests/testthat/test-unit-1.R
+++ tests/testthat/test-unit-1.R
@@ -50,12 +50,94 @@ test_that("Query conversions work.", {
   expect_true(nrow(genus_lookup("\"Drosophila\"")) >= 2)
 })
 
-test_that("Literal and identifier constructors work.", {
+test_that("Literal constructors work.", {
   lking_lear      = literal(text_value = "King Lear",        lang = "en")
   expect_match(represent(lking_lear), "\"King Lear\"@en")
   lshakespeare         = literal(text_value = "Shakespeare")
   expect_match(represent(lshakespeare), "\"Shakespeare\"")
   l1599 = literal(text_value = "1599", xsd_type = xsd_integer)
+  # TODO expect_match(squote(l1599), "\"1599\""^^xsd:integer') w00t?
   expect_match(l1599$text_value, "1599")
   expect_match(l1599$xsd_type$uri, "<http://www.w3.org/2001/XMLSchema#integer>")
+})
+
+
+test_that("Identifier constructors work", {
+  graphdb = rdf4r::basic_triplestore_access(
+    server_url = "http://graph.openbiodiv.net:7777",
+    user = "dbuser",
+    password = "public-access",
+    repository = "obkms_i6"
+  )
+  prefixes = c(
+    rdfs = "http://www.w3.org/2000/01/rdf-schema#",
+    rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    example = "http://rdflib-rdf4r.net/",
+    art = "http://art-ontology.net/"
+  )
+  art = prefixes[4]
+  eg = prefixes[3]
+  artist = identifier(id = "Artist", prefix = art)
+
+  expect_match(class(artist), "identifier")
+  expect_match(artist$uri, "<http://art-ontology.net/Artist>")
+
+  lking_lear      = literal(text_value = "King Lear",        lang = "en")
+
+  p_query = "SELECT DISTINCT ?id WHERE {
+    ?id rdfs:label %label
+  }"
+
+  simple_lookup = query_factory(p_query, access_options = graphdb)
+  lookup_or_mint_id = identifier_factory(fun = list(simple_lookup),
+                                         prefixes = prefixes,
+                                         def_prefix = eg)
+
+  expect_match(class(lookup_or_mint_id), "function")
+
+  idking_lear = lookup_or_mint_id(list(lking_lear))
+
+  expect_match(class(idking_lear), "identifier")
+})
+
+
+test_that("RDF generation works", {
+  graphdb = rdf4r::basic_triplestore_access(
+    server_url = "http://graph.openbiodiv.net:7777",
+    user = "dbuser",
+    password = "public-access",
+    repository = "obkms_i6"
+  )
+  prefixes = c(
+    rdfs = "http://www.w3.org/2000/01/rdf-schema#",
+    rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    example = "http://rdflib-rdf4r.net/",
+    art = "http://art-ontology.net/"
+  )
+  art = prefixes[4]
+  eg = prefixes[3]
+  wrote = identifier(id = "wrote", prefix = art)
+  p_query = "SELECT DISTINCT ?id WHERE {
+    ?id rdfs:label %label
+  }"
+
+  simple_lookup = query_factory(p_query, access_options = graphdb)
+  lookup_or_mint_id = identifier_factory(fun = list(simple_lookup),
+                                         prefixes = prefixes,
+                                         def_prefix = eg)
+
+  lshakespeare         = literal(text_value = "Shakespeare")
+  idshakespeare = lookup_or_mint_id(list(lshakespeare))
+
+  lking_lear      = literal(text_value = "King Lear",        lang = "en")
+  idking_lear = lookup_or_mint_id(list(lking_lear))
+
+  classics_rdf = ResourceDescriptionFramework$new()
+
+  expect_match(class(classics_rdf), "ResourceDescriptionFramework", all = FALSE)
+  expect_true(classics_rdf$add_triple(subject = idshakespeare,    predicate = wrote,      object = idking_lear))
+
+  classics_rdf$set_context(idshakespeare)
+  expect_match(class(classics_rdf$serialize()), "character")
+
 })
