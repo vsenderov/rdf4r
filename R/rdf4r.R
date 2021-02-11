d05eacec0c00c45238a8397f3305411effc7ee8a
Raúl Ortega <raul-ortega@users.noreply.github.com>
2021-02-11 19:04:26 +0100

    Update rdf4r.R

--- R/rdf4r.R
+++ R/rdf4r.R
@@ -1,437 +1,69 @@
-#' Mutable RDF Object
+#' RDF4R: R Library for Working with RDF
 #'
-#' This is a mutable object. Adds, stores, and serializes RDF statements
-#' efficiently. Uses an amortized vector \code{DynVector} as a internal
-#' storage. You can inherit this class and override to create your own
-#' implementation and still retain syntax compatibility.
+#' RDF4R provides the following facilities.
 #'
-#' @param initialize(size=100) Constructor. Use this to create new objects
-#'   it has a default tuning parameter. You may want to set it to the average
-#'   number of triples per processed document.
+#' @section Connection to a triple store:
 #'
-#' @param set_context(context) Context needs to be an \code{identifier}
-#'   object that correspond to the named graph of where the statements
-#'   are stored.
+#' \itemize{
+#'   \item \code{\link{basic_triplestore_access}}: connect to a triple store without a password or with a username and a password
+#'   \item \code{\link{api_triplestore_access}}: connect to a triple store with an API key
+#'   \item \code{\link{get_protocol_version}}:  Get Protocol Version
+#'   \item \code{\link{list_repositories}}: list the repositories at an endpoint
+#' }
 #'
-#' @param get_prefixes() Returns the prefixes as a deduplicated named
-#'   character vector of prefix to namespace mappings.
+#' @section Working with repositories on a triple store:
 #'
-#' @param add_triple(subject,predicate,object) \itemize{
-#'   \item{\code{subject} \code{identifier}}
-#'   \item{\code{predicate} \code{identifier}}
-#'   \item{\code{object} \code{identifier} or \code{literal} or \code{AnonymousRDF}.
-#'   Use the anonymous RDF if you hop through a blank node.}
-#' } Returns the success status (\code{logical})
+#' \itemize{
+#'   \item \code{\link{submit_sparql}}: Submit a SPARQL Query to a Triplestore (READ)
+#'   \item \code{\link{submit_sparql_update}}: Submit a SPARQL Query to a Triplestore (UPDATE)
+#'   \item \code{\link{add_data}}: Add Data to a Repository
+#' }
 #'
-#' @param add_triples(ll) ll needs to be a \code{ResourceDescriptionFramework}
-#'   object. The information is merged.
+#' @section Function factories to convert SPARQL queries, or data endpoints to R functions:
 #'
-#' @param add_triples_extended(data, subject_column_label = "", subject_column_name = "", subject_rdf_prefix = "", predicate, object_column_label = "", object_column_name = "", object_rdf_prefix = "", progress_bar = TRUE, append=FALSE) file_name needs to be characters, and
-#' progress_bar needs to be boolean.
+#' \itemize{
+#'   \item \code{\link{query_factory}}: Convert a parameterized SPARQL query to an R function
+#'   \item \code{\link{add_data_factory}}: Create a function that submits serialized RDF to a specific endpoint
+#' }
 #'
-#' @param set_list(triple_vector) triple_vector needs to be a \code{DynVector}
-#'   object. The information is merged.
+#' @section Working with literals and identifiers:
 #'
-#' @param serialize() Returns the Turtle serialization.
+#' \itemize{
+#'   \item \code{\link{literal}}: Family of functions for creating literals
+#'   \item \code{\link{identifier}}: Family of functions for creating resource identifiers
+#'   \item \code{\link{fidentifier}}: Identifier Constructor via a List of Lookup Functions
+#'   \item \code{\link{identifier_factory}}: Manufacturing Identifier Constructors
+#' }
 #'
-#' @param serialize_to_file(file_name,progress_bar=TRUE) file_name needs to be characters, and
-#' progress_bar needs to be boolean.
+#' @section Prefix management:
 #'
-#' @param prefix_list \code{DynVector} Prefixes of all the stored
-#'   identifiers as an uncollapsed list. For most cases you would want
-#'   to use \code{get_prefixes}.
+#' Prefixes are managed autmatically by RDF objects, so you probably wouldn't need to call the functions in this section manually.
+#' \itemize{
+#'   \item \code{\link{prefix_serializer}}: Serializes a Prefix Vector
+#' }
 #'
-#' @param context \code{identifier}. The named graph of where the statements
-#'   are stored.
+#' @section Creation and serialization of RDF:
 #'
-#' @export
-#' @family rdf
-ResourceDescriptionFramework = R6::R6Class(
-  classname = "ResourceDescriptionFramework",
-
-  public = list(
-
-    triples_list = NULL, # list of DynVector
-
-    prefix_list = NULL, # DynVector
-    context = NULL, # identifer
-
-    initialize = function(size = 1000000)
-    {
-      private$triples = DynVector$new(size = size)
-      self$prefix_list = DynVector$new(size = size)
-      self$triples_list <- list()
-    },
-
-    set_context = function(context)
-    {
-      stopifnot(is.identifier(context))
-      self$context = context
-    },
-
-    get_prefixes = function()
-    {
-      un = unlist(self$prefix_list$get())
-      un[!duplicated(un)]
-    },
-
-    add_triple = function(subject, predicate, object)
-    {
-      if (!is.identifier(subject) || !is.identifier(predicate) || !(is.literal(object) || is.identifier(object) || is.ResourceDescriptionFramework(object))) {
-        return (FALSE)
-      }
-      else {
-        self$prefix_list$add(subject$prefix)
-        self$prefix_list$add(predicate$prefix)
-        if (is.identifier(object)) {
-          self$prefix_list$add(object$prefix)
-        }
-        private$triples$add(list(subject = subject, predicate = predicate, object = object))
-        return(TRUE)
-      }
-    },
-
-    add_triples = function(ll)
-    {
-
-      if(!is.ResourceDescriptionFramework(ll)) return (FALSE)
-      if(length(ll$get_list()) == 0) return (FALSE)
-      else {
-        self$prefix_list$add_list(ll$prefix_list$get())
-        private$triples$add_list(ll$get_list())
-      }
-    },
-
-    get_list = function()
-    {
-      private$triples$get()
-    },
-
-    set_list = function(triple_vector)
-    {
-      private$triples = triple_vector
-    },
-
-    serialize = function()
-    {
-      if (is.null(self$context)) {
-        error("context not set. cannot serialize")
-      }
-      if (length(self$get_list()) == 0) {
-        return("")
-      }
-      # TODO prepend the prefiexes
-      serialization = DynVector$new(10)
-
-      serialization$add(
-        prefix_serializer(self$get_prefixes(), lang = "Turtle")
-        )
-
-      serialization$add(c(paste(self$context$qname, "{\n")))
-      # qnames of subjects and kick out NULL
-      subjects = sapply(private$triples$get(), function(t)
-      {
-        t$subject$qname
-      })
-
-      next_object = FALSE
-      for (s in unique(subjects)) {
-        couplet = private$write_couplet(subject = s, triples = private$triples$get())
-        if (next_object == FALSE) {
-          serialization$add_list(couplet$get())
-          next_object = TRUE
-        }
-        else{
-          serialization$add(c(". \n"))
-          serialization$add_list(couplet$get())
-        }
-      }
-      serialization$add(". }")
-      return (unlist(serialization$get()))
-    },
-    serialize_to_file = function(file_name, progress_bar=TRUE){
-      # write prefixes
-      cat(paste0(prefix_serializer(self$get_prefixes(), lang = "Turtle")), file = file_name)
-
-      # write context
-      cat(paste0(self$context$qname," {"),file = file_name, append = TRUE, sep="\n")
-      m_triples <- 0
-
-      for(triple_list in self$triples_list){
-        m_triples <- m_triples + length(triple_list$get())
-      }
-
-      if(isTRUE(progress_bar)){
-        pb <- txtProgressBar(1, m_triples, style = 3)
-      }
-
-      n <- 0
-      for(triple_list in self$triples_list){
-        n_triples <- length(triple_list)
-        for(triple in triple_list$get()) {
-          n <- n + 1
-          if(isTRUE(progress_bar)){
-            setTxtProgressBar(pb, n)
-          }
-          if (is.literal(triple$object)) {
-            the_object <- triple$object$squote
-          }
-          else if (is.identifier(triple$object)) {
-            the_object <- triple$object$qname
-          }
-          # writ triple to file
-          this_triple <- paste0(" ", triple$subject$qname, " ", triple$predicate$qname, " ", the_object," .\n")
-          cat(this_triple, file = file_name, append = TRUE)
-        }
-      }
-      # write }
-      cat(" }", file = file_name, append = TRUE)
-    },
-    add_triples_extended = function(data, subject_column_label = "", subject_column_name = "", subject_rdf_prefix = "", predicate, object_column_label = "", object_column_name = "", object_rdf_prefix = "", progress_bar = TRUE, append=FALSE){
-      # define resource identifiers for subjects and objects:
-      n_rows = nrow(data)
-      if(isTRUE(progress_bar)) {
-        pb <- txtProgressBar(1, n_rows, style = 3)
-      }
-      #phathe_triples <- private$triples
-      #if(isFALSE(append)){
-        phathe_triples <- DynVector$new(size = n_rows)
-      #}
-
-      if(subject_rdf_prefix != "") self$prefix_list$add(subject_rdf_prefix)
-      if(object_rdf_prefix != "") self$prefix_list$add(object_rdf_prefix)
-
-      for (i in 1:n_rows) {
-
-        the_subject <- if(subject_column_name == "")
-          literal(text_value = subject_column_label)
-        else
-          identifier(paste0(subject_column_label,data[i,subject_column_name]), prefix = subject_rdf_prefix)
-
-        the_object <- if(object_column_name == "")
-          literal(text_value = object_column_label)
-        else
-          identifier(paste0(object_column_label,data[i,object_column_name]), prefix = object_rdf_prefix)
-
-        # build triples:
-        phathe_triples$add(list(subject = the_subject, predicate = predicate, object = the_object))
-        if(isTRUE(progress_bar)) {
-          setTxtProgressBar(pb, i)
-        }
-      }
-      #self$set_list(triple_vector = phathe_triples)
-      n <- length(self$triples_list)
-      self$triples_list[[n+1]] <- phathe_triples
-    }
-  ),
-
-
-
-
-
-
-
-
-
-  private = list(
-
-    triples = NULL, # DynVector
-
-    # --- Serialization Functions ---
-    write_couplet = function(subject, triples)
-    {
-      local_serialization = DynVector$new(10)
-      local_serialization$add(c(paste(subject, " ")))
-      # subset the triples with only this subject
-      triples = lapply(triples, function(t)
-      {
-        if (t$subject$qname == subject) return(t)
-      })
-      triples = triples[!sapply(triples,is.null)]
-      # find the unique predicates
-      predicates = (sapply(triples, function (t)
-      {
-        t$predicate$qname
-      }))
-
-      next_object = FALSE
-      for (p in unique( predicates )) {
-        predicate_stanza = private$write_predicate_stanza(p, triples)
-        if (next_object == FALSE) {
-          local_serialization$add_list(predicate_stanza$get())
-          next_object = TRUE
-        }
-        else{
-          local_serialization$add(c(";\n\t"))
-          local_serialization$add_list(predicate_stanza$get())
-        }
-      }
-
-      return(local_serialization)
-    },
-
-
-
-
-
-
-
-
-
-
-    write_predicate_stanza = function(predicate, triples)
-    {
-      local_serialization = DynVector$new(10)
-      local_serialization$add(c(predicate, " "))
-      # subset only for this predicate
-      triples = lapply(triples, function (t) {
-        if (t$predicate$qname == predicate) return (t)
-      })
-      triples = triples[!sapply(triples,is.null)]
-      # We fucking do care about uniqueness of objects!!!!!!!
-      objects = lapply(triples, function (t) {
-        t$object
-      })
-      next_object = FALSE
-      for (o in unique(objects) ) {
-        end_stanza = private$write_end_stanza( o, triples )
-        if (next_object == FALSE) {
-          local_serialization$add_list(end_stanza$get())
-          next_object = TRUE
-        }
-        else {
-          local_serialization$add(c(", "))
-          local_serialization$add_list(end_stanza$get())
-        }
-      }
-      return (local_serialization)
-    },
-
-
-
-
-
-
-
-
-
-
-    write_end_stanza = function (object, triples)
-    {
-      local_serialization = DynVector$new(10)
-      if (is.literal(object)) {
-        local_serialization$add(object$squote)
-      }
-      else if (is.identifier(object)) {
-        local_serialization$add(object$qname)
-      }
-      else {
-        # object is RDF with blank nodes --> recursion
-        local_serialization$add(c(" [ "))
-        local_serialization$add_list(private$write_couplet(subject = blank_node, triples = object))
-        local_serialization$add(c(" ] "))
-      }
-      return(local_serialization)
-    }
-
-  )
-)
-
-
-
-
-#' Is the object an Triples List (RDF)?
+#' \itemize{
+#'   \item \code{\link{ResourceDescriptionFramework}}: Mutable RDF Object
+#'   \item \code{\link{AnonRDF}}: Create a list of RDF statements that all share the same blank subject node
+#' }
 #'
-#' @param x object to check
+#' @section A basic vocabulary of semantic elements:
 #'
-#' @return logical
+#' \itemize{
+#'   \item \code{\link{semantic_elements}}: Predifined resource identifiers from widely-spread ontologies
+#' }
+
+
+#' @section Function factories to convert SPARQL queries, or data endpoints to R functions:
 #'
-#' @export
-#' @family rdf
-is.ResourceDescriptionFramework = function(x)
-{
-  if ("ResourceDescriptionFramework" %in% class(x)) TRUE
-  else FALSE
-}
-
-
-
-
-
-#' Create a list of RDF statements that all share the same blank subject node
-#' @export
-#' @family anonymous rdf
-AnonRDF = R6::R6Class(
-  classname = "anonymous_rdf",
-  inherit = ResourceDescriptionFramework,
-
-  public = list(
-
-    add_triple = function(predicate, object)
-    {
-      if (!is.identifier(predicate) || !is.list(object)) {
-        return (FALSE);
-      }
-      else {
-        super$add(list(subject = blank_node, predicate = predicate, object = object))
-        return(TRUE)
-      }
-    },
-
-    add_triples = function(ll)
-    {
-      if(!is.AnonRDF(ll)) return (FALSE)
-      else {
-        self$add_list(ll$get())
-      }
-    },
-
-    serialize = function(context)
-    {
-      # you cannot serialize anonymous RDF
-      return (FALSE)
-    }
-  )
-)
-
-
-
-#' Is the object an Anonymous Triples List (RDF)?
-#' @export
-#' @family anonymous rdf
-is.AnonRDF = function(x)
-{
-  if ("anonymous_rdf" %in% class(x)) TRUE
-  else FALSE
-}
-
-
-
-
-
-
-
-#' RDF Class with \code{rdflib} backend
+#' \itemize{
+#'   \item \code{\link{add_trig_file_to_graphdb}}: Retrieves rdf data from a file and imports it to graphdb
+#' }
 #'
-#' @inheritParams ResourceDescriptionFramework
+
 #'
-#' @export
-RdfLibBackend = R6::R6Class(
-  inherit = ResourceDescriptionFramework,
-  classname = "RdfLibBackend",
-
-  public = list(
-
-    initialize = function(context)
-    {
-      super$initialize()
-      super$set_context(context)
-    },
-
-    nquad = function(subject, predicate, object, context)
-    {
-      paste(represent(subject), represent(predicate), represent(object), represent(self$context), ".")
-    }
-
-  )
-)
+#' @docType package
+#' @name rdf4r
+NULL
